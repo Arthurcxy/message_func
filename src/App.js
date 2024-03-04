34bf9bd4354c93bc30673d91b6e7f1193196efc9
DESKTOP-59PVHCP\DDD <dck>
2024-03-04 22:41:34 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// routes
+import Router from "./routes";
+// theme
+import ThemeProvider from './theme';
+
+function App() {
+  return (
+    <ThemeProvider>
+        {" "}
+        <Router />{" "}
+    </ThemeProvider>
+  );
+}
+
+export default App;
