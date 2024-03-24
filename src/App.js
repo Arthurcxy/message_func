9fa8862c7f79b71acd9f3268b6225b418f3339af
DESKTOP-59PVHCP\DDD <dck>
2024-03-25 00:46:02 +0800

    全局引入 sendbird 聊天sdk，connect链接

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 // routes
-import Router from "./routes";
+import Router from './routes';
 // theme
 import ThemeProvider from './theme';
-
+import SendbirdProvider from '@sendbird/uikit-react/SendbirdProvider';
+import SendbirdChat from '@sendbird/chat';
+import { GroupChannelModule } from '@sendbird/chat/groupChannel';
 function App() {
-  return (
-    <ThemeProvider>
-        {" "}
-        <Router />{" "}
-    </ThemeProvider>
-  );
+    console.log(process.env);
+    const { REACT_APP_ID, REACT_APP_ACCESS_TOKEN, REACT_APP_USER_ID, REACT_APP_NICKNAME } =
+        process.env;
+
+    const sb = SendbirdChat.init({
+        appId: REACT_APP_ID,
+        modules: [new GroupChannelModule()]
+    });
+    console.log(sb);
+    sb.connect(REACT_APP_USER_ID, REACT_APP_ACCESS_TOKEN);
+    return (
+        <ThemeProvider>
+            <SendbirdProvider
+                appId={REACT_APP_ID}
+                userId={REACT_APP_USER_ID}
+                nickname={REACT_APP_NICKNAME}
+                accessToken={REACT_APP_ACCESS_TOKEN}>
+                {' '}
+                <Router  />{' '}
+            </SendbirdProvider>
+        </ThemeProvider>
+    );
 }
 
 export default App;
