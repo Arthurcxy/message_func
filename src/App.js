cc6dd7c0ca334951811f5a512515989e653e5638
DESKTOP-59PVHCP\DDD <dck>
2024-03-30 23:04:43 +0800

    新增全局provider 包裹

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SendbirdProvider from '@sendbird/uikit-react/SendbirdProvider';
 import SendbirdChat from '@sendbird/chat';
 import { GroupChannelModule } from '@sendbird/chat/groupChannel';
 import { message } from 'antd';
+import { ConfigProvider } from 'antd';
 function App() {
     // console.log(process.env);
     const { REACT_APP_ID, REACT_APP_ACCESS_TOKEN, REACT_APP_USER_ID, REACT_APP_NICKNAME } =
@@ -13,29 +14,35 @@ function App() {
     const sb = SendbirdChat.init({
         appId: REACT_APP_ID,
         modules: [new GroupChannelModule()],
-        token:'22fa8ed3271e9747e28febc618863a0801324697'
+        token: '22fa8ed3271e9747e28febc618863a0801324697'
     });
     console.log(sb);
-    sb.connect(REACT_APP_USER_ID, REACT_APP_ACCESS_TOKEN).then(res=>{
+    sb.connect(REACT_APP_USER_ID, REACT_APP_ACCESS_TOKEN).then(res => {
         console.log(res);
-        if(res.isActive){
-            message.success('Welcome Back' )
-        }else{
+        if (res.isActive) {
+            message.success('Welcome Back');
+        } else {
             message.error('Sendbird 连接失败: ');
         }
     });
     return (
-        <ThemeProvider>
-          
-            <SendbirdProvider
-                appId={REACT_APP_ID}
-                userId={REACT_APP_USER_ID}
-                nickname={REACT_APP_NICKNAME}
-                accessToken={REACT_APP_ACCESS_TOKEN}>
-                {' '}
-                <Router  />{' '}
-            </SendbirdProvider>
-        </ThemeProvider>
+        <ConfigProvider theme={{
+            token: {
+              /* 这里是你的全局 token */
+              colorPrimary:'#5955B3'
+            },
+          }}>
+            <ThemeProvider>
+                <SendbirdProvider
+                    appId={REACT_APP_ID}
+                    userId={REACT_APP_USER_ID}
+                    nickname={REACT_APP_NICKNAME}
+                    accessToken={REACT_APP_ACCESS_TOKEN}>
+                    {' '}
+                    <Router />{' '}
+                </SendbirdProvider>
+            </ThemeProvider>
+        </ConfigProvider>
     );
 }
 
